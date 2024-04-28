1e849c4cf798974a3fd075713d7a1487d0499f9d
kangho lee <ibcylon@naver.com>
2024-04-28 12:31:07 +0900

    Initial Commit

---
+++ LocationExercise/LocationExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationExerciseApp.swift
+//  LocationExercise
+//
+//  Created by kangho lee on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
